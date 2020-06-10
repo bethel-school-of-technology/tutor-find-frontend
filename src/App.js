91fe6eded0922c08dc497d20f538a15820f766d9
Taylor Rasay <taykainoa@gmail.com>
2020-06-09 18:46:20 -0700

    3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 import Navbar from "./components/navbar.component";
 import SubjectList from "./components/subjects-list.component";
 import EditSubject from "./components/edit-subjects.components";
 import CreateSubject from "./components/create-subject.component";
 import CreateUser from "./components/create-user.component";
 import HomePage from "./components/HomePage";
- 
+import SessionsList from "./components/sessions-list.component";
+
 function App() {
   return (
     <Router>
@@ -17,10 +17,11 @@ function App() {
         <Navbar />
         <br />
         <Route path="/" exact component={HomePage} />
-        <Route path="/subjects-list" component={SubjectList}/>
+        <Route path="/subjects-list" component={SubjectList} />
         <Route path="/edit/:id" component={EditSubject} />
         <Route path="/create" component={CreateSubject} />
         <Route path="/user" exct component={CreateUser} />
+        <Route path="/sessions" exact component={SessionsList} />
       </div>
     </Router>
   );
