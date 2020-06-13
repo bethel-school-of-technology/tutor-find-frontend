e3b8ace47b6dbd8ba868a93c15789acc1bd39476
Taylor Rasay <taykainoa@gmail.com>
2020-06-12 18:50:32 -0700

    css styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 export default class HomePage extends Component {
   render() {
     return (
-    <div className="homepage">
-        {this.props.children} 
-
-           <lu>Welcome to the Community </lu>
-          <p></p>
-           <li>create</li>
-           <li>connect</li>
-           <li>converse</li>
-<p></p>
-          <h3>TUTORFIND</h3>
-          <h3>A fanastic new plateform for people in the communicty to connect with each other.</h3>
-          <h3> This connection hub will allow tutors to freelance their skills and for students to find help in their studies quickly and safely.</h3>
-          <h3> The goal is to bring people together in their communities with common interests, peace and the love for helping people.  </h3>
+      <div className="homepage">
+        {this.props.children}
+        <ul className="kyle">Welcome to the Community </ul>
         <p></p>
-        </div>
-
-      
-
-    
-
-    )
-   
+        <h2 className="joe">
+          create&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;connect&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;converse
+        </h2>
+        <p></p>
+        <h3 className="chris">TUTORFIND</h3>
+        <h3 className="text">
+          A fanastic new platform for people in the communicty to connect with
+          each other.
+        </h3>
+        <h3 className="text">
+          {" "}
+          This connection hub will allow tutors to freelance their skills and
+          for students to find help in their studies quickly and safely.
+        </h3>
+        <h3 className="text">
+          {" "}
+          The goal is to bring people together in their communities with common
+          interests, peace and the love for helping people.{" "}
+        </h3>
+        <p></p>
+      </div>
+    );
   }
 }
 
-{/* <div class="footer">
+{
+  /* <div class="footer">
   <p>Footer</p>
-</div> */}
+</div> */
+}
