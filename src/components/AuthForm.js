f0f83d795a6c90f8d3c81537b85e617efa24afa3
terelin <bravebeauty28@gmail.com>
2020-06-24 20:02:56 -0400

    edited login

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -19,16 +19,16 @@ const Form = styled.div`
 
 const Input = styled.input`
   padding: 1rem;
-  border: 1px solid #999;
+  border: 2px solid #999;
   margin-bottom: 1rem;
   font-size: 0.8rem;
 `;
 
 const Button = styled.button`
-  background: linear-gradient(to bottom, #6371c7, #5563c1);
-  border-color: #3f4eae;
-  border-radius: 3px;
-  padding: 1rem;
+  background: red;
+  border-color: #black;
+  border-radius: 2px;
+  padding: -1rem;
   color: white;
   font-weight: 700;
   width: 100%;
@@ -42,7 +42,7 @@ const Logo = styled.img`
 `;
 
 const Error = styled.div`
-  background-color: red;
+  background-color: grey;
 `;
 
 export { Form, Input, Button, Logo, Card, Error };
